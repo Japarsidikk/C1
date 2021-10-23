7a38968f04dcd409ce2c28da6880e40b1a066939
japarsidikk <radenrivan2@gmail.com>
2021-10-23 19:29:20 +0700

    first commit

--- pustaka-booking/application/config/config.php
+++ pustaka-booking/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/LATIHAN_CI';
+$config['base_url'] = 'http://localhost/C1/pustaka-booking/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/LATIHAN_CI';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
